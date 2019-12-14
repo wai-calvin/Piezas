cd6db91d12c53b96e87035b8e990365a6933dbd9
wai-calvin <cwai14@live.com>
2019-12-13 22:13:29 -0800

    Implemented Piezas

--- Piezas.cpp
+++ Piezas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -17,11 +17,13 @@
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+  turn = X;
+  board.resize(BOARD_ROWS, std::vector<Piece>(BOARD_COLS, Blank));
 }
 
 /**
@@ -30,19 +32,42 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+  for(int i=0; i<BOARD_ROWS; i++) {
+    for(int j=0; j<BOARD_COLS; j++) {
+      board[i][j] = Blank;
+    }
+  }
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+  Piece currentTurn = turn;
+  if(turn == X) {
+    turn = O;
+  }
+  else {
+    turn = X;
+  }
+
+  if(column < 0 || column >= BOARD_COLS)  {
+    return Invalid;
+  }
+
+  for(int i=0; i<BOARD_ROWS; i++){
+    if(board[i][column] == Blank){
+      board[i][column] = currentTurn;
+      return currentTurn;
+    }
+  }
+  return Blank;
 }
 
 /**
@@ -51,13 +76,17 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+  if(column < 0 || column >= BOARD_COLS || row < 0 || row >= BOARD_ROWS){
+    return Invalid;
+  }
+
+  return board[row][column];
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
@@ -65,5 +94,76 @@ Piece Piezas::pieceAt(int row, int column)
 **/
 Piece Piezas::gameState()
 {
-    return Blank;
+  int xTallyMax = 0;
+  int oTallyMax = 0;
+
+  Piece previousSpace;
+  int currTally = 0;
+
+  for(int i=0; i<BOARD_COLS; i++) {
+    previousSpace = Blank;
+    for(int j=0; j<BOARD_ROWS; j++) {
+      if(board[j][i] == Blank){
+        return Invalid;
+      }
+
+      if(board[j][i] == previousSpace) {
+        currTally++;
+        if(board[j][i] == X) {
+          if(currTally > xTallyMax) {
+            xTallyMax = currTally;
+          }
+        }
+
+        if(board[j][i] == O) {
+          if(currTally > oTallyMax) {
+            oTallyMax = currTally;
+          }
+        }
+      }
+      else {
+        currTally=0;
+      }
+      previousSpace = board[j][i];
+   }
+}
+
+for(int i=0; i<BOARD_ROWS; i++) {
+  previousSpace = Blank;
+  for(int j=0; j<BOARD_COLS; j++) {
+    if(board[i][j] == Blank) {
+      return Invalid;
+    }
+    if(board[i][j] == previousSpace) {
+      currTally++;
+      if(board[i][j] == X) {
+        if(currTally > xTallyMax) {
+          xTallyMax = currTally;
+        }
+      }
+
+      if(board[i][j] == O) {
+        if(currTally > oTallyMax) {
+          oTallyMax = currTally;
+        }
+      }
+    }
+    else {
+      currTally=0;
+    }
+    previousSpace = board[i][j];
+   }
+ }
+
+  if(xTallyMax == oTallyMax) {
+     return Blank;
+  }
+
+  if(xTallyMax > oTallyMax) {
+     return X;
+  }
+
+  else {
+    return O;
+  }
 }
